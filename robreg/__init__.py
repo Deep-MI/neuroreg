58b1a20e79695917c2c97be463160d9820613420
Martin Reuter <reuter@mit.edu>
2025-04-02 17:57:22 +0200

    temporarily remove _version for local ipynb testing

--- robreg/__init__.py
+++ robreg/__init__.py
@@ -1,3 +1,3 @@
-from ._version import __version__  # noqa: F401
-from .register import register, register_pyramid  # noqa: F401
-from .utils._config import sys_info  # noqa: F401
+#from robreg._version import __version__  # noqa: F401
+from robreg.register import register, register_pyramid  # noqa: F401
+from robreg.utils._config import sys_info  # noqa: F401
