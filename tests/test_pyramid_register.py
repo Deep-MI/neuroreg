4715bfaf85560f80edad1e27d3c7f43969f258ed
Martin Reuter <reuter@mit.edu>
2026-03-31 21:20:25 +0200

    remove simple tests

--- tests/test_pyramid_register.py
+++ tests/test_pyramid_register.py
@@ -34,48 +34,6 @@ def _make_img(
     return nib.Nifti1Image(data, affine)
 
 
-class TestPyramidHelpers:
-    def test_get_pyramid_limits_basic(self):
-        min_steps, max_steps = get_pyramid_limits(torch.Size([64, 64, 64]), minsize=16)
-        assert int(min_steps.item()) == 0
-        assert int(max_steps.item()) == 2
-
-    def test_get_pyramid_limits_includes_exact_minsize_boundary(self):
-        min_steps, max_steps = get_pyramid_limits(torch.Size([32, 32, 32]), minsize=16)
-        assert int(min_steps.item()) == 0
-        assert int(max_steps.item()) == 1
-
-    def test_get_pyramid_limits_keeps_exact_maxsize_boundary(self):
-        min_steps, max_steps = get_pyramid_limits(torch.Size([128, 128, 128]), minsize=16, maxsize=64)
-        assert int(min_steps.item()) == 1
-        assert int(max_steps.item()) == 3
-
-    def test_build_gaussian_pyramid_shapes_and_affines(self):
-        image = torch.rand(32, 32, 32)
-        affine = torch.eye(4)
-        imgs, affines = build_gaussian_pyramid(image, affine, limits=(torch.tensor(0), torch.tensor(1)))
-
-        assert [tuple(img.shape) for img in imgs] == [(32, 32, 32), (16, 16, 16)]
-        assert len(affines) == 2
-        assert torch.allclose(affines[0], affine)
-        assert affines[1][0, 0].item() == pytest.approx(2.0)
-        assert affines[1][1, 1].item() == pytest.approx(2.0)
-        assert affines[1][2, 2].item() == pytest.approx(2.0)
-
-    def test_build_gaussian_pyramid_updates_affine_for_odd_sizes(self):
-        image = torch.rand(5, 5, 5)
-        affine = torch.eye(4)
-        imgs, affines = build_gaussian_pyramid(image, affine, limits=(torch.tensor(0), torch.tensor(1)))
-
-        assert [tuple(img.shape) for img in imgs] == [(5, 5, 5), (2, 2, 2)]
-        assert affines[1][0, 0].item() == pytest.approx(2.5)
-        assert affines[1][1, 1].item() == pytest.approx(2.5)
-        assert affines[1][2, 2].item() == pytest.approx(2.5)
-        assert affines[1][0, 3].item() == pytest.approx(0.75)
-        assert affines[1][1, 3].item() == pytest.approx(0.75)
-        assert affines[1][2, 3].item() == pytest.approx(0.75)
-
-
 class TestRegisterPyramidSynthetic:
     def test_register_pyramid_returns_v2v_on_identical_images(self):
         img = _make_img()
